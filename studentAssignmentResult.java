65cf0604b8090e1ab3065bb763e5c2b3007fb05a
prabigya09 <63800081+prabigya09@users.noreply.github.com>
2023-09-28 09:32:27 +1000

    importing filereader, arraylists, util scanner

---
+++ studentAssignmentResult.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class studentAssignmentResult here.
+ *
+ * @author (Prabigya)
+ * @version (Assignment2)
+ */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class studentAssignmentResult
+{
+    
+}
